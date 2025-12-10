6d917e99a1547c8c3e3a806f56271b101a5e9224
JohanCacerez <johancacerez10@gmail.com>
2025-12-10 11:10:52 -0600

    adapter: redis para tokens activos

---
+++ app/infrastructure/cahce/redis_token_repository.py
@@ -0,0 +1,16 @@
+import redis
+from app.infrastructure.config.settings import REDIS_HOST, REDIS_PORT, JWT_EXPIRE_SECONDS
+
+class RedisTokenRepository:
+
+    def __init__(self):
+        self.client = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, decode_responses=True)
+
+    def save_token(self, token: str, user_id: str):
+        self.client.setex(token, JWT_EXPIRE_SECONDS, user_id)
+
+    def get_user_id(self, token: str):
+        return self.client.get(token)
+    
+    def delete_token(self, token: str):
+        self.client.delete(token)
