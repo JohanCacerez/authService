123ab9d4fc1c3a7b6deb4598e6c82beda6b915c9
JohanCacerez <johancacerez10@gmail.com>
2025-12-09 19:03:02 -0600

    fix

--- app/infrastructure/config/settings.py
+++ app/infrastructure/config/settings.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 MONGO_URI = os.getenv("MONGO_URI")
 REDIS_HOST = os.getenv("REDIS_HOST", "redis")
-REDIS_HOST = int(os.getenv("REDIS_HOST", 6379))
+REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
 
 JWT_SECRET =os.getenv("JWT_SECRET", "mysecret")
 JWT_EXPIRE_SECONDS = int(os.getenv("JWT_EXPIRE_SECONDS", 3600))
