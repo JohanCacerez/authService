f4b651dbf436c8127f6014520f7fb121cc29062f
JohanCacerez <johancacerez10@gmail.com>
2025-12-09 18:58:59 -0600

    variables de entorno

---
+++ app/infrastructure/config/settings.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MONGO_URI = os.getenv("MONGO_URI")
+REDIS_HOST = os.getenv("REDIS_HOST", "redis")
+REDIS_HOST = int(os.getenv("REDIS_HOST", 6379))
+
+JWT_SECRET =os.getenv("JWT_SECRET", "mysecret")
+JWT_EXPIRE_SECONDS = int(os.getenv("JWT_EXPIRE_SECONDS", 3600))
